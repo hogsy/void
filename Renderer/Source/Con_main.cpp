742701ddd90d1a7144e1141a724f4987d6eea94c
jms <jms>
2000-04-14 02:48:12 +0000

    set renderer logfile in debug mode

--- Renderer/Source/Con_main.cpp
+++ Renderer/Source/Con_main.cpp
@@ -35,10 +35,10 @@ CRConsole::CRConsole(I_ExeConsole * p_eCons): m_seperatorchar('^'),
 
 #if RENDERER_LOGFILE 
 	//open logfile
-	char f[260];
-	strcpy(f, rInfo->working_dir);
-	strcat(f, "\\console.log");
-	m_logfile = fopen(f, "w");
+//	char f[260];
+//	strcpy(f, rInfo->working_dir);
+//	strcat(f, "\\console.log");
+	m_logfile = fopen("E:\\Void\\Console.log", "w");
 #endif
 }
 
