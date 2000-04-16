22d8e1cf5669fd8bb7649e8b397d1de15bc0ef45
jms <jms>
2000-04-16 02:12:02 +0000

    *** empty log message ***

---
+++ vLight/Source/std_lib.h
@@ -0,0 +1,21 @@
+
+
+#ifndef STD_LIB_H
+#define STD_LIB_H
+
+#pragma warning(disable : 4018)     // signed/unsigned mismatch
+#pragma warning(disable : 4305)     // truncate from double to float
+
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+
+void Error(char *err, ...);
+void v_printf(char *msg, ...);
+
+
+
+
+#endif
